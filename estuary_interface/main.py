7ad2a997ee8abad098b5fc69393300d197e635a2
tubleronchik <m.bystramovich@gmail.com>
2022-03-17 23:47:21 +0300

    added submit content function. add comments

--- estuary_interface/main.py
+++ estuary_interface/main.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from urllib import request
+import re
 import requests
 import json
 import argparse
 import typing as tp
+import logging
+
+logger = logging.getLogger("estuary_interface")
+logger.propagate = False
+logger.level = "INFO"
 
 
-def set_headers() -> tp.Tuple[dict]:
+def _set_headers() -> tp.Tuple[dict]:
     auth_header = {"Authorization": f"Bearer {token}"}
     json_header = {
         "Authorization": f"Bearer {token}",
@@ -21,26 +26,76 @@ def set_headers() -> tp.Tuple[dict]:
     return (auth_header, json_header, form_header)
 
 
-def add_cid(cid: str):
-    auth_header, json_header, form_header = set_headers()
+def add_cid(cid: str) -> str:
+    """
+    Take an existing IPFS CID, and make storage deals for it.
+    """
+    auth_header, json_header, form_header = _set_headers()
     data = json.dumps(
         {
             "name": f"{cid}",
             "root": f"{cid}",
         }
     )
+    params = json.dumps({"ignore-dupes": "true"})
     response = requests.post(
         url="https://api.estuary.tech/content/add-ipfs",
         data=data,
         headers=json_header,
+        params=params,
     )
     print(response.text)
+
+
+def _get_upload_endpoints() -> tp.List[str]:
+    """
+    Get endpoints for uploading data
+    """
+    auth_header, json_header, form_header = _set_headers()
+    response = requests.get("https://api.estuary.tech/viewer", headers=auth_header)
+    endpoints_list = json.loads(response.text)["settings"]["uploadEndpoints"]
+    return endpoints_list
+
+
+def add_content(file_path: str) -> str:
+    """
+    Upload file to Estuary Node. One file at a time.
+    """
+    auth_header, json_header, form_header = _set_headers()
+    body = {"data": (open(file_path, "rb"))}
+    response = requests.post(
+        url="https://api.estuary.tech/content/add",
+        files=body,
+        headers=form_header,
+    )
+    print(response.text)
+    if "error" in json.loads(response.text).keys():
+        if json.loads(response.text)["error"] == "ERR_CONTENT_ADDING_DISABLED":
+            endpoints = _get_upload_endpoints()
+            for link in endpoints:
+                response = requests.post(
+                    url=link,
+                    files=body,
+                    headers=form_header,
+                )
+                if response.status_code == 200:
+                    break
+                print(response.text)
+        else:
+            logger.warning(f"Error submitting data: {response.text}")
+    print(response.text)
+    if response.status_code == 200:
+        return f"Data was uploaded successfully. CID is: {json.loads(response.text)['cid']}"
 
 
 def run():
     global token
     parser = argparse.ArgumentParser(description="Add API token for Estuary.")
-    parser.add_argument("token", type=str, help="API token")
+    parser.add_argument(
+        "token",
+        type=str,
+        help="API token",
+    )
     args = parser.parse_args()
     token = args.token
 
